809e1e0f83a665f054664f4d08b6de19ba1e887b
Hemmy <hema.kappa55@gmail.com>
2018-10-31 05:22:12 +0200

    Updates

---
+++ precache-manifest.f049dda754e6424ac68954d15143f0ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd05dfe8a20f405ed35",
+    "url": "/graphql-book/static/js/runtime~main.abd05dfe.js"
+  },
+  {
+    "revision": "f0eea703359795efec44",
+    "url": "/graphql-book/static/js/main.f0eea703.chunk.js"
+  },
+  {
+    "revision": "61edd763e426b76ee319",
+    "url": "/graphql-book/static/js/1.61edd763.chunk.js"
+  },
+  {
+    "revision": "f0eea703359795efec44",
+    "url": "/graphql-book/static/css/main.fb175c1a.chunk.css"
+  },
+  {
+    "revision": "1a8bf71d10fc05b3e265db0c78a7365f",
+    "url": "/graphql-book/index.html"
+  }
+];
